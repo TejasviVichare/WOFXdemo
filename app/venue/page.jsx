2ab29efc5b3d3fa300faada47704365c9f63f55c
finesse-mumbai <web@finesse.co.in>
2024-04-16 18:24:38 +0530

    added venue page

--- app/venue/page.jsx
+++ app/venue/page.jsx
@@ -1,8 +1,37 @@
-
-
+import Image from "next/image"
+import venueImage from '../../public/assests/venue2023.png'
 function page() {
   return (
-    <div>page</div>
+    <div className='mx-auto   w-full max-w-screen-2xl '>
+        <div className="mt-20  flex justify-center">
+
+      <div className="space">
+      <center>
+        <h1 className="text-2xl font-bold">VENUE</h1>
+      </center>
+
+      <p><a style={{border:"1px solid black"}} className="px-4 py-3 font-semibold text-black bg-gray-300  rounded"  href="">LOCATION</a></p>
+      <p>&nbsp;</p>
+      <p className="font-bold">Bombay Exhibition Center, Mumbai, India</p>
+      <div className="flex py-4">
+        <div className="w-1/2">
+        <Image
+          className="venueImages"
+          src={venueImage}
+          alt="image1"
+        />
+        </div>
+        <iframe className="w-1/2"
+          src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d120609.80431251878!2d72.853645!3d19.149009!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3be7b651a8eb7135%3A0xa618573a34efa52e!2sNesco%20Pvt%20Ltd!5e0!3m2!1sen!2sus!4v1693827946866!5m2!1sen!2sus"
+          allowfullscreen=""
+          loading="lazy"
+          referrerpolicy="no-referrer-when-downgrade"
+        ></iframe>
+      </div>
+    </div>
+
+    </div>
+    </div>
   )
 }
 
