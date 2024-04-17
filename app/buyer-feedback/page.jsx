372296c5f12f6ad687338b43a775eed55a1fd21c
sushilkumar666 <sushilssharma064@gmail.com>
2024-04-17 18:13:35 +0530

    sushil's changes

--- app/buyer-feedback/page.jsx
+++ app/buyer-feedback/page.jsx
@@ -1,5 +1,313 @@
+import Image from "next/image";
+
 function page() {
-  return <div>page this the buyer feedback page </div>;
+  return (
+    <>
+      <div className="bg-gray-200 mb-16 w-[80%] text-center m-auto">
+        <div className=" p-8 text-4xl"> Buyer Testimonials</div>
+        <div className="pb-16  ">
+          <div id="row" className="md:pl-16 md:pr-16 flex flex-wrap">
+            <div
+              id="card"
+              className="flex flex-col md:w-[30%] p-8 m-3 items-center  bg-white rounded-lg align-top"
+            >
+              <div className="mb-3">
+                <Image
+                  src="/assests/buyer-feedback/Shiv Shankar.png"
+                  alt="sdjfld"
+                  width={120}
+                  height={120}
+                />
+              </div>
+              <div className="text-xl m-1">Shiv Shankar</div>
+              <div className="text-lg font-bold leading-6 m-1 mb-3">
+                Purchase Head, <br /> Durian industries Ltd.
+              </div>
+              <div className="text-gray-600 text-[14px]">
+                WOFX has been a good experience as it enabled us to meet
+                international suppliers from Malaysia, Thailand, Indonesia and
+                others. For us the added advantage is that we have good
+                franchise options which they share. WOFX is up to international
+                levels with good participants and displays with creativity at
+                its best.
+              </div>
+            </div>
+            <div
+              id="card"
+              className="flex flex-col md:w-[30%] p-8 m-3 items-center  bg-white rounded-lg  "
+            >
+              <div className="mb-3">
+                <Image
+                  src="/assests/buyer-feedback/pallavi.png"
+                  alt="sdjfld"
+                  width={120}
+                  height={120}
+                />
+              </div>
+              <div className="text-xl m-1">Pallavi Nanda</div>
+              <div className="text-lg font-bold leading-6 m-1 mb-3">
+                Design Head, <br /> The Phoenix Mills Ltd
+              </div>
+              <div className="text-gray-600 text-[14px]">
+                The international designs and products on display, especially
+                designs from Thailand and Indonesia were of interest to us. It
+                is great to see these kinds of options now available in India
+                thanks to WOFX. Overall, I found WOFX to be very informative for
+                industry professionals
+              </div>
+            </div>
+            <div
+              id="card"
+              className="flex flex-col md:w-[30%] p-8 m-3 items-center  bg-white rounded-lg  "
+            >
+              <div className="mb-3">
+                <Image
+                  src="/assests/buyer-feedback/Urooj Ansar.png"
+                  alt="sdjfld"
+                  width={120}
+                  height={120}
+                />
+              </div>
+              <div className="text-xl m-1">Urooj Ansar</div>
+              <div className="text-lg font-bold leading-6 m-1 mb-3">
+                Manager, <br /> Firstcry
+              </div>
+              <div className="text-gray-600 text-[14px]">
+                WOFX is a great show where I got to meet many suppliers. The
+                variety and quality of products on display at the expo were
+                excellent.
+              </div>
+            </div>
+          </div>
+          <div id="row" className="md:pl-16 md:pr-16 flex flex-wrap">
+            <div
+              id="card"
+              className="flex flex-col md:w-[30%] p-8 m-3 items-center  bg-white rounded-lg"
+            >
+              <div className="mb-3">
+                <Image
+                  src="/assests/buyer-feedback/Manoj.png"
+                  alt="sdjfld"
+                  width={120}
+                  height={120}
+                />
+              </div>
+              <div className="text-xl m-1">Manoj Lodha</div>
+              <div className="text-lg font-bold leading-6 m-1 mb-3">
+                CEO, <br /> Sonali Chairs
+              </div>
+              <div className="text-gray-600 text-[14px]">
+                The networking opportunities at WOFX were very good as I found
+                potential companies to work and trade with. While I was
+                specifically looking for office furniture, I was pleased to also
+                discover various other categories of furniture which I found
+                very useful to keep in mind for future expansion.
+              </div>
+            </div>
+            <div
+              id="card"
+              className="flex flex-col md:w-[30%] p-8 m-3 items-center  bg-white rounded-lg"
+            >
+              {" "}
+              <div className="mb-3">
+                <Image
+                  src="/assests/buyer-feedback/zuhair.png"
+                  alt="sdjfld"
+                  width={120}
+                  height={120}
+                />
+              </div>
+              <div className="text-xl m-1">Zuhair E'layan</div>
+              <div className="text-lg font-bold leading-6 m-1 mb-3">
+                General Manager, <br /> Regal Furniture Global
+              </div>
+              <div className="text-gray-600 text-[14px]">
+                This is our first visit to WOFX. This is an excellent experience
+                as we made deals with the exhibitors here and found potential
+                companies to do business with. We look forward to visiting again
+                and doing more business next year.
+              </div>
+            </div>
+            <div
+              id="card"
+              className="flex flex-col md:w-[30%] p-8 m-3 items-center  bg-white rounded-lg"
+            >
+              {" "}
+              <div className="mb-3">
+                <Image
+                  src="/assests/buyer-feedback/Rohan.png"
+                  alt="sdjfld"
+                  width={120}
+                  height={120}
+                />
+              </div>
+              <div className="text-xl m-1">Rohan Nalawade</div>
+              <div className="text-lg font-bold leading-6 m-1 mb-3">
+                Sr. Design Manager, <br /> Chandak Group
+              </div>
+              <div className="text-gray-600 text-[14px]">
+                The overall show was good. We would also recommend WOFX to our
+                business network as they had displayed many good products from
+                India as well as from countries around the world.
+              </div>
+            </div>
+          </div>
+          <div id="row" className="md:pl-16 md:pr-16 flex flex-wrap">
+            <div
+              id="card"
+              className="flex flex-col md:w-[30%] p-8 m-3 items-center  bg-white rounded-lg"
+            >
+              {" "}
+              <div className="mb-3">
+                <Image
+                  src="/assests/buyer-feedback/Sameer.png"
+                  alt="sdjfld"
+                  width={120}
+                  height={120}
+                />{" "}
+              </div>
+              <div className="text-xl m-1">Sameer Alsurkhi</div>
+              <div className="text-lg font-bold leading-6 m-1 mb-3">
+                Commercial Manager, <br /> Firenze Furniture, UAE
+              </div>
+              <div className="text-gray-600 text-[14px]">
+                We specialize in office furniture and were looking for more
+                products to expand our range. Our visit to WOFX was definitely
+                productive due to the variety, collections as well as
+                international manufacturers showcased here especially those from
+                Singapore and other ASEAN countries.
+              </div>
+            </div>
+            <div
+              id="card"
+              className="flex flex-col md:w-[30%] p-8 m-3 items-center  bg-white rounded-lg"
+            >
+              {" "}
+              <div className="mb-3">
+                <Image
+                  src="/assests/buyer-feedback/Puneet.png"
+                  alt="sdjfld"
+                  width={120}
+                  height={120}
+                />{" "}
+              </div>
+              <div className="text-xl m-1">Puneet Sethi</div>
+              <div className="text-lg font-bold leading-6 m-1 mb-3">
+                Principal Architect, <br /> Planner & Valuer, IAD Studio, India
+              </div>
+              <div className="text-gray-600 text-[14px]">
+                I’m quite happy with the products I’ve seen over the last 3 days
+                from India and from across the world. WOFX is very useful as it
+                keeps us informed of new developments, innovations and
+                technologies from across the world. I’m pleased with the way
+                WOFX has been organized.
+              </div>
+            </div>
+            <div
+              id="card"
+              className="flex flex-col md:w-[30%] p-8 m-3 items-center  bg-white rounded-lg"
+            >
+              {" "}
+              <div className="mb-3">
+                <Image
+                  src="/assests/buyer-feedback/Rajesh.png"
+                  alt="sdjfld"
+                  width={120}
+                  height={120}
+                />
+              </div>
+              <div className="text-xl m-1">Rajesh Sangewar</div>
+              <div className="text-lg font-bold leading-6 m-1 mb-3">
+                Head - Product Design & Development, <br /> Wipro, India
+              </div>
+              <div className="text-gray-600 text-[14px]">
+                Though my visit was brief, I found the international pavilion at
+                the fair particularly useful. Connecting with fabric vendors and
+                talented outdoor furniture designers and suppliers was a great
+                investment of my time, and I'm excited to explore potential
+                collaborations.
+              </div>
+            </div>
+          </div>
+          <div id="row" className="md:pl-16 md:pr-16 flex flex-wrap">
+            <div
+              id="card"
+              className="flex flex-col md:w-[30%] p-8 m-3 items-center  bg-white rounded-lg"
+            >
+              <div className="mb-3">
+                <Image
+                  src="/assests/buyer-feedback/Asad Tarifi.png"
+                  alt="sdjfld"
+                  width={120}
+                  height={120}
+                />
+              </div>
+              <div className="text-xl m-1">Asad Tarifi</div>
+              <div className="text-lg font-bold leading-6 m-1 mb-3">
+                General Manager <br /> Zahrat Amman Industries, UAE
+              </div>
+              <div className="text-gray-600 text-[14px]">
+                This is the 1st time I have come to Mumbai and I am very pleased
+                to visit WOFX as I found what I was looking for over here. WOFX
+                is great not only for bringing manufacturers and suppliers from
+                across the world but also for presenting a range of furniture
+                from these countries. I was pleased to connect with many
+                companies whom I am interested to trade with.
+              </div>
+            </div>
+            <div
+              id="card"
+              className="flex flex-col md:w-[30%] p-8 m-3 items-center  bg-white rounded-lg"
+            >
+              <div className="mb-3">
+                <Image
+                  src="/assests/buyer-feedback/danube home.png"
+                  alt="sdjfld"
+                  width={120}
+                  height={120}
+                />
+              </div>
+              <div className="text-xl m-1">Santosh Sonar</div>
+              <div className="text-lg font-bold leading-6 m-1 mb-3">
+                Senior Buyer, <br /> Danube Home, UAE
+              </div>
+              <div className="text-gray-600 text-[14px]">
+                As a Furniture buyer from Dubai, I’m always on the lookout for
+                fresh perspective and innovative designs. WOFX – World Furniture
+                Expo provided that! The mix of international and domestic
+                exhibitors was perfect. I met and placed orders with trendy
+                furniture designers from Southeast Aisa and with established
+                Indian manufacturers. This Expo is a must attend for any serious
+                furniture buyer.
+              </div>
+            </div>
+            <div
+              id="card"
+              className="flex flex-col md:w-[30%] p-8 m-3 items-center  bg-white rounded-lg"
+            >
+              <div className="mb-3">
+                <Image
+                  src="/assests/buyer-feedback/urban ladder.png"
+                  alt="sdjfld"
+                  width={120}
+                  height={120}
+                />
+              </div>
+              <div className="text-xl m-1">Pranal Kapadni</div>
+              <div className="text-lg font-bold leading-6 m-1 mb-3">
+                Design Consultant, <br /> Urban Ladder, India
+              </div>
+              <div className="text-gray-600 text-[14px]">
+                I thoroughly enjoyed the meticulously organized WOFX 2023 event.
+                It stood out as an exemplary showcase of excellence and
+                innovation in the furniture industry.
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 }
 
 export default page;
