185ef047eb143902a9a3a8ed58d71095ae450330
finesse-mumbai <web@finesse.co.in>
2024-04-18 10:25:40 +0530

    fixing form

--- app/exhibitor_enquiry/page.jsx
+++ app/exhibitor_enquiry/page.jsx
@@ -12,7 +12,7 @@ const MyForm = () => {
     name: '',
     designation: '',
     company_name: '',
-    product_cat: [], 
+    mpc:[], 
     nob: [], 
     product_cat_other: '',
     mobile: '',
@@ -176,23 +176,23 @@ const MyForm = () => {
 
                 <p className='text-lg mt-4'><strong>Major Exhibit Category*</strong></p>
                 <div className="grid grid-cols-1 md:grid-cols-3">
-                  <label className="checkbox col-sm-12"><input type="checkbox" name="product_cat[]" className='Apparel Fabric' value='Home' id="2" onChange={handleChange} />&nbsp;  Home</label>
-                  <label className="checkbox col-sm-12"><input type="checkbox" name="product_cat[]" className='exhibit_country' value='Office' onChange={handleChange} /> Office</label>
-                  <label className="checkbox col-sm-12"><input type="checkbox" name="product_cat[]" className='exhibit_country' value='Outdoor' onChange={handleChange} />&nbsp; Outdoor</label>
-                  <label className="checkbox col-sm-12"><input type="checkbox" name="product_cat[]" className='exhibit_country' value='Plastic' onChange={handleChange} />&nbsp; Plastic</label>
-                  <label className="checkbox col-sm-12"><input type="checkbox" name="product_cat[]" className='exhibit_country' value='Kid' onChange={handleChange} />&nbsp;  Kids</label>
-                  <label className="checkbox col-sm-12"><input type="checkbox" name="product_cat[]" className='exhibit_country' value='Airport' onChange={handleChange} />&nbsp;  Airport</label>
-                  <label className="checkbox col-sm-12"><input type="checkbox" name="product_cat[]" className='exhibit_country' value='Theatre / Auditorium' onChange={handleChange} />&nbsp;   Theatre / Auditorium</label>
-                  <label className="checkbox col-sm-12"><input type="checkbox" name="product_cat[]" className='exhibit_country' value='Hospital' onChange={handleChange} />&nbsp;   Hospital</label>
-                  <label className="checkbox col-sm-12"><input type="checkbox" name="product_cat[]" className='exhibit_country' value='Public' onChange={handleChange} />&nbsp;   Public</label>
-                  <label className="checkbox col-sm-12"><input type="checkbox" name="product_cat[]" className='exhibit_country' value='Hotel & Restaurant' onChange={handleChange} />&nbsp;   Hotel & Restaurant</label>
-                  <label className="checkbox col-sm-12"><input type="checkbox" name="product_cat[]" className='exhibit_country' value='Accessories' onChange={handleChange} />&nbsp;   Accessories</label>
-                  <label className="checkbox col-sm-12"><input type="checkbox" name="product_cat[]" className='exhibit_country' value='Décor' onChange={handleChange} />&nbsp;   Décor</label>
-                  <label className="checkbox col-sm-12"><input type="checkbox" name="product_cat[]" className='exhibit_country' value='Modular' onChange={handleChange} />&nbsp;   Modular</label>
-                  <label className="checkbox col-sm-12"><input type="checkbox" name="product_cat[]" className='exhibit_country' value='Furniture Fittings' onChange={handleChange} />&nbsp;   Furniture Fittings</label>
-                  <label className="checkbox col-sm-12"><input type="checkbox" name="product_cat[]" className='exhibit_country' value='Mattresses' onChange={handleChange} />&nbsp;   Mattresses</label>
-                  <label className="checkbox col-sm-12"><input type="checkbox" name="product_cat[]" className='exhibit_country' value='Lighting' onChange={handleChange} />&nbsp;   Lighting</label>
-                  <label className="checkbox col-sm-12"><input type="checkbox" name="product_cat[]" className='exhibit_country' value='Others' onChange={handleChange} />&nbsp;   Others (please mention)</label>
+                  <label className="checkbox col-sm-12"><input type="checkbox" name="mpc[]" className='Apparel Fabric' value='Home' id="2" onChange={handleChange} />&nbsp;  Home</label>
+                  <label className="checkbox col-sm-12"><input type="checkbox" name="mpc[]" className='exhibit_country' value='Office' onChange={handleChange} /> Office</label>
+                  <label className="checkbox col-sm-12"><input type="checkbox" name="mpc[]" className='exhibit_country' value='Outdoor' onChange={handleChange} />&nbsp; Outdoor</label>
+                  <label className="checkbox col-sm-12"><input type="checkbox" name="mpc[]" className='exhibit_country' value='Plastic' onChange={handleChange} />&nbsp; Plastic</label>
+                  <label className="checkbox col-sm-12"><input type="checkbox" name="mpc[]" className='exhibit_country' value='Kid' onChange={handleChange} />&nbsp;  Kids</label>
+                  <label className="checkbox col-sm-12"><input type="checkbox" name="mpc[]" className='exhibit_country' value='Airport' onChange={handleChange} />&nbsp;  Airport</label>
+                  <label className="checkbox col-sm-12"><input type="checkbox" name="mpc[]" className='exhibit_country' value='Theatre / Auditorium' onChange={handleChange} />&nbsp;   Theatre / Auditorium</label>
+                  <label className="checkbox col-sm-12"><input type="checkbox" name="mpc[]" className='exhibit_country' value='Hospital' onChange={handleChange} />&nbsp;   Hospital</label>
+                  <label className="checkbox col-sm-12"><input type="checkbox" name="mpc[]" className='exhibit_country' value='Public' onChange={handleChange} />&nbsp;   Public</label>
+                  <label className="checkbox col-sm-12"><input type="checkbox" name="mpc[]" className='exhibit_country' value='Hotel & Restaurant' onChange={handleChange} />&nbsp;   Hotel & Restaurant</label>
+                  <label className="checkbox col-sm-12"><input type="checkbox" name="mpc[]" className='exhibit_country' value='Accessories' onChange={handleChange} />&nbsp;   Accessories</label>
+                  <label className="checkbox col-sm-12"><input type="checkbox" name="mpc[]" className='exhibit_country' value='Décor' onChange={handleChange} />&nbsp;   Décor</label>
+                  <label className="checkbox col-sm-12"><input type="checkbox" name="mpc[]" className='exhibit_country' value='Modular' onChange={handleChange} />&nbsp;   Modular</label>
+                  <label className="checkbox col-sm-12"><input type="checkbox" name="mpc[]" className='exhibit_country' value='Furniture Fittings' onChange={handleChange} />&nbsp;   Furniture Fittings</label>
+                  <label className="checkbox col-sm-12"><input type="checkbox" name="mpc[]" className='exhibit_country' value='Mattresses' onChange={handleChange} />&nbsp;   Mattresses</label>
+                  <label className="checkbox col-sm-12"><input type="checkbox" name="mpc[]" className='exhibit_country' value='Lighting' onChange={handleChange} />&nbsp;   Lighting</label>
+                  <label className="checkbox col-sm-12"><input type="checkbox" name="mpc[]" className='exhibit_country' value='Others' onChange={handleChange} />&nbsp;   Others (please mention)</label>
                 </div>
                 <p>{errors.product_cat && <span className="text-red-500">This field is required.</span>}</p>
 
