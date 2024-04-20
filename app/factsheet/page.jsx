2acfb481a8f582671d1cebd0db953da471642988
sushilkumar666 <sushilssharma064@gmail.com>
2024-04-20 12:27:08 +0530

    sushils changes

--- app/factsheet/page.jsx
+++ app/factsheet/page.jsx
@@ -1,9 +1,347 @@
-
-
 function page() {
   return (
-    <div>page</div>
-  )
+    <>
+      <div>&nbsp;</div>
+      <div>&nbsp;</div>
+      <div>&nbsp;</div>
+      <table align="center" className="w-[70%] valign-top ">
+        <tbody className="valign-top">
+          <tr className="bg-[#6cced9]">
+            <td
+              colSpan={3}
+              className="p-4  text-lg font-medium  text-white "
+              align="center"
+              cellpadding="0"
+            >
+              FACTSHEET
+            </td>
+          </tr>
+
+          <tr>
+            <td className="pt-2 pb-2">
+              <p> Date </p>
+            </td>
+            <td className="pt-2 pb-2" colSpan={2}>
+              <p>5-6-7 December 2024</p>
+            </td>
+          </tr>
+
+          <tr className="border-b-[1px] border-gray-400  leading-8">
+            <td style={{ verticalAlign: "top" }}>
+              <p> Exhibition Timings </p>
+            </td>
+            <td>
+              <p>Fair Date</p>
+              <p>5 Dec (Thu.)</p>
+              <p>6 Dec (Fri.)</p>
+              <p>7 Dec (Sat.)</p>
+            </td>
+            <td>
+              <p>Opening Hours</p>
+              <p>10.00 a.m. - 6.00 p.m.</p>
+              <p>10.00 a.m. - 6.00 p.m.</p>
+              <p>10.00 a.m. - 5.00 p.m.</p>
+            </td>
+          </tr>
+          <tr className="border-b-[1px] border-gray-400 p-4  ">
+            <td className="pt-2 pb-2">
+              <p> Venue</p>
+            </td>
+            <td className="pt-2 pb-2" colSpan={2}>
+              <p>Bombay Exhibition Center, Mumbai, India</p>
+            </td>
+            <td />
+          </tr>
+          <tr className="border-b-[1px] border-gray-400   ">
+            <td className="pt-2 pb-2">
+              <p> Organizer </p>
+            </td>
+            <td className="pt-2 pb-2" colSpan={2}>
+              <p> Worldex India Exhibition &amp; Promotion Pvt. Ltd.</p>
+            </td>
+          </tr>
+          <tr className="border-b-[1px] border-gray-400   ">
+            <td className="pt-2 pb-2">
+              <p> Format </p>
+            </td>
+            <td>
+              <p className="pt-2 pb-2">Business to Business</p>
+            </td>
+            <td />
+          </tr>
+          <tr className="border-b-[1px] border-gray-400 leading-8  ">
+            <td
+              style={{ verticalAlign: "top" }}
+              className="pt-2 pb-2 valign-top"
+            >
+              <p> Major Exhibit Categories</p>
+            </td>
+            <td className="pt-2 pb-2">
+              <ul className="list-ul">
+                <p>
+                  <bold>FURNITURE</bold>
+                </p>
+                <p>• &nbsp; Home</p>
+                <p>• &nbsp; Office</p>
+                <p>• &nbsp; Outdoor</p>
+                <p>• &nbsp; Modular</p>
+                <p>• &nbsp; Kids</p>
+                <p>• &nbsp; Handcrafted</p>
+              </ul>
+            </td>
+            <td className="pt-2 pb-2">
+              <ul className="list-ul">
+                <br />
+                <p>• &nbsp; Plastic</p>
+                <p>• &nbsp; Public</p>
+                <p>• &nbsp; School/College</p>
+                <p>• &nbsp; Institutional</p>
+                <p>• &nbsp; Mattresses</p>
+                <p>• &nbsp; Décor</p>
+              </ul>
+            </td>
+          </tr>
+          <tr className="border-b-[1px] border-gray-400  leading-8 ">
+            <td style={{ verticalAlign: "top" }} className="pt-2 pb-2">
+              Major Exhibiting Countries/ Regions
+            </td>
+            <td className="pt-2 pb-2">
+              <ul className="list-ul">
+                <p>• &nbsp; India</p>
+                <p>• &nbsp; Sri Lanka</p>
+                <p>• &nbsp; Bangladesh</p>
+                <p>• &nbsp; Malaysia</p>
+                <p>• &nbsp; Thailand</p>
+                <p>• &nbsp; Indonesia</p>
+                <p>• &nbsp; Vietnam</p>
+                <p>• &nbsp; Philippines</p>
+              </ul>
+            </td>
+            <td className="pt-2 pb-2">
+              <ul className="list-ul">
+                <p>• &nbsp; Hong Kong,</p>
+                <p>• &nbsp; China</p>
+                <p>• &nbsp; Singapore</p>
+                <p>• &nbsp; Turkiye</p>
+                <p>• &nbsp; Italy</p>
+                <p>• &nbsp; France</p>
+                <p>• &nbsp; USA</p>
+                <p>&nbsp;&nbsp;&nbsp;&nbsp;amongst others</p>
+              </ul>
+            </td>
+          </tr>
+          <tr className="border-b-[1px] border-gray-400  leading-8 ">
+            <td className="pt-2 pb-2" style={{ verticalAlign: "top" }}>
+              Thematic Zones
+            </td>
+            <td className="pt-2 pb-2" colSpan={2}>
+              <ul className="list-ul">
+                <p>• &nbsp; Avante Garde - Hall of Fame</p>
+                <p>• &nbsp; Grand Galleria - Hall of Elegance</p>
+                <p>• &nbsp; Design Edge - Hall of Innovations</p>
+                <p>• &nbsp; Asean Panorama</p>
+                <p>• &nbsp; SOI - States of India</p>
+              </ul>
+            </td>
+            <td />
+          </tr>
+          <tr className="border-b-[1px] border-gray-400 leading-8  ">
+            <td style={{ verticalAlign: "top" }} className="pt-2 pb-2">
+              Conferences, Design Ideas and Product Launches
+            </td>
+            <td className="pt-2 pb-2" colSpan={2}>
+              <ul className="list-ul">
+                <p>• &nbsp; WOW - Words of Wisdom</p>
+                <p>• &nbsp; Design Walk</p>
+                <p>• &nbsp; Design Talk</p>
+                <p>• &nbsp; Business Pulse</p>
+              </ul>
+            </td>
+            <td />
+          </tr>
+          <tr className="border-b-[1px] border-gray-400 leading-8  ">
+            <td style={{ verticalAlign: "top" }} className="pt-2 pb-2">
+              Buyer Admission
+            </td>
+            <td className="pt-2 pb-2" colSpan={2}>
+              <ul className="list-ul">
+                <p>
+                  • &nbsp; Entry strictly for trade buyers on production of
+                  valid business cards.
+                  <br /> &nbsp;&nbsp;&nbsp;&nbsp;(Pre-register online or onsite)
+                </p>
+                <p>• &nbsp; Rights of admission reserved.</p>
+                <p>• &nbsp; No registration Fee</p>
+                <p>• &nbsp; Entry below 18 years will not be permitted.</p>
+                <p>• &nbsp; No retail sale.</p>
+              </ul>
+            </td>
+          </tr>
+          <tr className="border-b-[1px] border-gray-400 leading-8  ">
+            <td style={{ verticalAlign: "top" }}>
+              <p>
+                Buyer Lounges <br />
+                For Business Meetings and Networking{" "}
+              </p>
+            </td>
+            <td colSpan={2}>
+              <table
+                width="100%"
+                border={1}
+                className=" align-top"
+                style={{
+                  borderCollapse: "collapse",
+                  verticalAlign: "top",
+                  border: "1px solid gray",
+                  marginTop: "20px",
+                  marginBottom: "20px",
+                }}
+              >
+                <tbody>
+                  <tr className="border-b-[1px] border-gray-400 pt-2 pb-2  ">
+                    <td className="text-center p-2  ">
+                      <img
+                        src="/assests/factsheet/Logo-factsheet-1.png"
+                        alt=""
+                        style={{ width: 120 }}
+                      />
+                    </td>
+                    <td className="p-2 text-center">
+                      <img
+                        src="/assests/factsheet/Logo-factsheet-2.png"
+                        alt=""
+                        style={{ width: 120 }}
+                      />
+                    </td>
+                    <td className="p-2 text-center">
+                      <img
+                        src="/assests/factsheet/Logo-factsheet-3.png"
+                        alt=""
+                        style={{ width: 120 }}
+                      />
+                    </td>
+                    <td className="p-2 text-center">
+                      <img
+                        src="/assests/factsheet/Logo-factsheet-4.png"
+                        alt=""
+                        style={{ width: 120 }}
+                      />
+                    </td>
+                  </tr>
+                  <tr>
+                    <td style={{ textAlign: "center", verticalAlign: "top" }}>
+                      <p style={{ textAlign: "centers" }}>Dealers</p>
+                      <p style={{ textAlign: "centers" }}>Distributors</p>
+                      <p style={{ textAlign: "centers" }}>Wholesalers</p>
+                      <p style={{ textAlign: "centers" }}>Importers</p>
+                      <p style={{ textAlign: "centers" }}>
+                        Trading &amp; Buying Houses
+                      </p>
+                      <p style={{ textAlign: "centers" }}>Agents</p>
+                    </td>
+                    <td style={{ textAlign: "center", verticalAlign: "top" }}>
+                      <p>Retailers</p>
+                      <p>E-Tailers</p>
+                      <p>Large Format Retailers</p>
+                      <p>Online Traders</p>
+                    </td>
+                    <td style={{ textAlign: "center", verticalAlign: "top" }}>
+                      <p>Franchisees</p>
+                      <p>Entrepreneurs</p>
+                      <p>Agents</p>
+                      <p>Consultants</p>
+                    </td>
+                    <td style={{ textAlign: "center", verticalAlign: "top" }}>
+                      <p>Architects</p>
+                      <p>Interior Designers</p>
+                      <p>Real Estate Developers</p>
+                      <p>HORECA</p>
+                      <p>Corporate Sourcing Heads</p>
+                      <p>Institutional</p>
+                    </td>
+                  </tr>
+                </tbody>
+              </table>
+            </td>
+          </tr>
+          <tr className="border-b-[1px] border-gray-400 leading-8  ">
+            <td className="pt-2 pb-2">Website</td>
+            <td className="pt-2 pb-2">
+              <p>
+                <a
+                  className="text-black no-underline"
+                  href="www.wofxworldexpo.com"
+                >
+                  www.wofxworldexpo.com
+                </a>{" "}
+              </p>
+            </td>
+            <td />
+          </tr>
+          <tr className="border-b-[1px] border-gray-400 leading-8  ">
+            <td style={{ verticalAlign: "top" }} className="pt-2 pb-2">
+              <p className="sp-line">
+                For Registrations, <br /> Enquiries contact
+              </p>
+            </td>
+            <td className="pt-2 pb-2">
+              <p className="sp-line">
+                Rampat Gupta <br />
+                <span>
+                  <a className="contactDetail" href="tel: +91 9819567106">
+                    +91 9819567106
+                  </a>
+                </span>
+                <br />
+                <span>
+                  <a
+                    className="contactDetail mail"
+                    href="mailto: rampatgupta@worldexindia.com"
+                  >
+                    rampatgupta@worldexindia.com
+                  </a>
+                </span>
+              </p>
+            </td>
+            <td className="pt-2 pb-2">
+              <p className="sp-line">
+                Haider Ali Mirza <br />
+                <span>
+                  <a className="contactDetail" href="tel: +91 9619095955">
+                    +91 9619095955
+                  </a>
+                </span>
+                <br />
+                <span>
+                  <a
+                    className="contactDetail mail"
+                    href="mailto: haider@worldexindia.com"
+                  >
+                    haider@worldexindia.com
+                  </a>
+                </span>
+              </p>
+            </td>
+          </tr>
+        </tbody>
+      </table>
+      <div>&nbsp;</div>
+      <div>&nbsp;</div>
+      <center>
+        <a
+          className="hover:bg-white hover:text-black"
+          href="https://wofxworldexpo.com/assets/pdf/factsheet.pdf"
+          target="_blank"
+        >
+          <button className="bg-black text-white p-2 hover:bg-gray-500 hover:text-white ">
+            Download
+          </button>
+        </a>
+      </center>
+      <div>&nbsp;</div>
+      <div>&nbsp;</div>
+    </>
+  );
 }
 
-export default page
+export default page;
