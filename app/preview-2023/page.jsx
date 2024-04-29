fcb4fac93e2d312bbe1798fbe3079aefcd1f9de3
finesse-mumbai <web@finesse.co.in>
2024-04-29 14:26:23 +0530

    fixing

--- app/preview-2023/page.jsx
+++ app/preview-2023/page.jsx
@@ -14,7 +14,7 @@ function page() {
             <div className="border py-4 hover:bg-slate-900">
               <Image className="post-show-img" src={preImage1} width={400} height="auto" alt="Preview 1" />
               <div className="text-center my-6">
-                <p className="text-center py-4 px-0 md:px-2 bg-orange-500 text-white font-semibold" href="/assets/pdf/WOFX-PREVIEW-1_press.pdf">
+                <p className="text-center py-4 px-0 md:px-2 bg-orange-500 text-white font-semibold" href="/assests/pdf/WOFX-PREVIEW-1_press.pdf">
                   Preview 1
                 </p>
               </div>
@@ -24,7 +24,7 @@ function page() {
             <div className="border py-4 hover:bg-slate-900">
               <Image className="post-show-img" src={preImage2} width={400} height="auto" alt="Preview 2" />
               <p className="text-center my-6">
-                <p className="text-center py-4 px-0 md:px-2 bg-orange-500 text-white font-semibold" href="/assets/pdf/WOFX-PREVIEW-2_press.pdf">
+                <p className="text-center py-4 px-0 md:px-2 bg-orange-500 text-white font-semibold" href="/assests/pdf/WOFX-PREVIEW-2_press.pdf">
                   Preview 2
                 </p>
               </p>
