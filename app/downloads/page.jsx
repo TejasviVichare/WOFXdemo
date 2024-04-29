335627b944ae4707512b903137d4d4bc0e130096
finesse-mumbai <web@finesse.co.in>
2024-04-29 17:35:38 +0530

    fixing issue in web

--- app/downloads/page.jsx
+++ app/downloads/page.jsx
@@ -9,12 +9,12 @@ function page() {
 
         <div className="flex justify-center flex-wrap gap-20">
           <div className="relative border px-4 py-4 rounded">
-            <center className="h-32"><Image src={wofxLogo} width={250} height={70} alt="wofx-logo" /></center>
-            <div className="w-full flex justify-center"><a className="text-center py-2 px-4  md:px-16 bg-orange-500 text-white font-semibold"  href="">Downlaod</a></div>
+            <center className="h-32"><Image src={wofxLogo} width={300} height={150} alt="wofx-logo" /></center>
+            <div className="w-full flex justify-center"><a className="text-center py-2 px-4  md:px-16 bg-orange-500 text-white font-semibold"  href="/assests/showSummeryLogo.png" download>Downlaod</a></div>
           </div>
           <div className="relative border px-4 py-4 rounded">
-            <center className="h-32"> <Image src={WORLDEX} width={250} height={70} alt="wofx-logo" /></center>
-            <div className="w-full flex justify-center"><a className="text-center py-2 px-2 md:px-16 bg-orange-500 text-white font-semibold"  href="">Downlaod</a></div>
+            <center className="h-32"> <Image src={WORLDEX} width={300} height={150} alt="wofx-logo" /></center>
+            <div className="w-full flex justify-center"><a className="text-center py-2 px-2 md:px-16 bg-orange-500 text-white font-semibold"  href="/assests/WORLDEX.png" download>Downlaod</a></div>
           </div>
         </div>
         <p>&nbsp;</p>
