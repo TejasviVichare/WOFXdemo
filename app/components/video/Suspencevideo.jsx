a52f953e5108c0b57302445c7fe2262cfa1630fb
TejasviVichare <tejasvivichare2121@gmail.com>
2024-05-02 12:22:11 +0530

    Video Updated

--- app/components/video/Suspencevideo.jsx
+++ app/components/video/Suspencevideo.jsx
@@ -9,7 +9,7 @@ function Suspencevideo() {
     <section className='videoContainer'>
       <div  className=' mx-auto flex w-full max-w-screen-2xl'>
         <Suspense fallback={<p>Loading......</p>}>
-        <Videocomponent videosrc='https://res.cloudinary.com/dl2eivpdr/video/upload/v1714373078/k0ifizbnfnizxsvi5vnz.mp4' /> 
+        <Videocomponent videosrc='https://res.cloudinary.com/dkcfsf9dz/video/upload/v1714632172/WOFX_4_nifcf6.mp4' /> 
         </Suspense>
       </div>
     </section>
