02196045e0c7f20c83275d0b16d68dd5aa9022b9
TejasviVichare <tejasvivichare2121@gmail.com>
2024-05-08 18:38:36 +0530

    Updated

--- app/components/video/Videocomponent.jsx
+++ app/components/video/Videocomponent.jsx
@@ -1,35 +1,25 @@
-'use client'
+"use client";
 import React, { useEffect, useState } from "react";
 import ReactPlayer from "react-player";
 
-const Videocomponent = ( {videosrc}) => {
-    const [isClient, setIsClient] = useState(false);
-    useEffect(() => {
-        setIsClient(true);
-    }, []);
+const Videocomponent = ({ videosrc }) => {
+  const [isClient, setIsClient] = useState(false);
+  useEffect(() => {
+    setIsClient(true);
+  }, []);
 
-    if (!isClient) {
-        return null; 
-    }
-    
+  if (!isClient) {
+    return null;
+  }
 
-    return (
-        <div className="bgvideo mx-auto  w-full max-w-screen-2xl">
-             <video
-          
-          muted
-          autoPlay
-          playsInline
-          loop
-        >
-          <source
-            src="assests/WOFX_4_nifcf6.mp4"
-            type="video/mp4"
-          />
-        </video>
-            <source src={videosrc} type="video/mp4" />
-        </div>
-    );
+  return (
+    <div className="bgvideo mx-auto  w-full max-w-screen-2xl">
+      <video muted autoPlay playsInline loop>
+        <source src="assests/WOFX_4_nifcf6.mp4" type="video/mp4" />
+      </video>
+      <source src={videosrc} type="video/mp4" />
+    </div>
+  );
 };
 
 export default Videocomponent;
