28734d2ef700e9d6a47634cdb52b605c426a38dd
TejasviVichare <tejasvivichare2121@gmail.com>
2024-05-14 11:26:53 +0530

    Image Replaced

---
+++ app/download-logo/page.jsx
@@ -0,0 +1,24 @@
+import Image from "next/image"
+import wofxbanner from '../../public/assests/wofxbanner.jpg'
+
+
+function page() {
+  return (
+    <div className='mx-auto  mt-40  w-full max-w-screen-2xl '>
+        <div className="flex justify-center flex-wrap gap-20">
+          <div className="relative border px-4 py-4 rounded mb-16 m-4">
+            <center >
+                <Image src={wofxbanner} width={500} height={500} alt="wofx-logo"  className="mb-8"/>
+            </center>
+            <div className="w-full flex justify-center">
+                <a className="text-center py-3 px-4  md:px-16  text-white font-semibold" style={{backgroundColor:'#E73E41'}}  href="/assests/wofxbanner.jpg" download>Downlaod</a>
+            </div>
+          </div>
+          
+        </div>
+      
+      </div>
+  )
+}
+
+export default page
