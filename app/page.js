52a4b37ea1c9e9b148db90dfc78f2a7cd65f172e
TejasviVichare <tejasvivichare2121@gmail.com>
2024-05-29 18:01:00 +0530

    video with poster

--- app/page.js
+++ app/page.js
@@ -1,4 +1,3 @@
-
 import Suspencevideo from "./components/video/Suspencevideo.jsx";
 import Aboutwofx from "./components/aboutwofx/Aboutwofx.jsx";
 import Wofxadvantage from "./components/wofxadvantage/Wofxadvantage.jsx";
@@ -8,62 +7,75 @@ import Industrypartnerslider from "./components/industrypartnerslider/Industrypa
 import InternationPavilion from "./components/Internationpavilion/InternationPavilion.jsx";
 import Buyer_lounges from "./components/buyer-lounges/Buyer_lounges.jsx";
 import Show_Summery from "./components/show-summery/Show_Summery.jsx";
-
+import { Suspense } from "react";
 
 export default function Home() {
   return (
     <>
-    <div className="videoContainer">
-    <div className="bgvideo mx-auto  w-full max-w-screen-2xl">
-      <video muted autoPlay playsInline loop>
-        <source src="assests/WOFX_4_nifcf6.mp4" type="video/mp4" />
-      </video>
-      <source src='assests/WOFX_4_nifcf6.mp4' type="video/mp4" />
-    </div> 
-    </div>
-    <p>&nbsp;</p>
-    <p>&nbsp;</p>
-    <p>&nbsp;</p>
-    <p>&nbsp;</p>
-    <p>&nbsp;</p>
-    <p>&nbsp;</p>
-    <p>&nbsp;</p>
-    <p>&nbsp;</p>
-    
-    <Aboutwofx />
-    <p>&nbsp;</p>
-    <p>&nbsp;</p>
-    <Show_Summery />
-    <p>&nbsp;</p>
-    <p>&nbsp;</p>
+      <Suspense fallback={<p>Loading....</p>}>
+        <div className="videoContainer">
+          <div className="bgvideo mx-auto  w-full max-w-screen-2xl">
+            <video
+              muted
+              autoPlay
+              playsInline
+              loop
+              poster="assests/posterimage.jpeg"
+            >
+              <source src="assests/WOFX_4_nifcf6.mp4" type="video/mp4" />
+            </video>
+            <source src="assests/WOFX_4_nifcf6.mp4" type="video/mp4" />
+          </div>
+        </div>
+      </Suspense>
+      <p>&nbsp;</p>
+      <p>&nbsp;</p>
+      <p>&nbsp;</p>
+      <p>&nbsp;</p>
+      <p>&nbsp;</p>
+      <p>&nbsp;</p>
+      <p>&nbsp;</p>
+      <p>&nbsp;</p>
 
-    <Wofxadvantage />
-    <p>&nbsp;</p>
-    <p>&nbsp;</p>
-    
- 
-    <Buyer_lounges />
-    <p>&nbsp;</p>
-    <p>&nbsp;</p>
-    <p>&nbsp;</p>
-    <p>&nbsp;</p>
-    <p>&nbsp;</p>
-    <center><h1 className="text-3xl">Past Featured Brands </h1></center>
-    <p>&nbsp;</p>
-    <CountrySlider />
-    <p>&nbsp;</p>
-    <p>&nbsp;</p>
-    <p>&nbsp;</p>
-    <p>&nbsp;</p>
-    <center><h1 className="text-3xl">Industry Bodies Endorsing WOFX</h1></center>
-    <p>&nbsp;</p>
-    <Industribody />
-    <p>&nbsp;</p>
-    {/* <Industrypartnerslider /> */}
-   
-    <center><h1 className="text-3xl py-6">International Pavilion Organisers </h1></center>
-    <InternationPavilion />
-    <p>&nbsp;</p>
+      <Aboutwofx />
+      <p>&nbsp;</p>
+      <p>&nbsp;</p>
+      <Show_Summery />
+      <p>&nbsp;</p>
+      <p>&nbsp;</p>
+
+      <Wofxadvantage />
+      <p>&nbsp;</p>
+      <p>&nbsp;</p>
+
+      <Buyer_lounges />
+      <p>&nbsp;</p>
+      <p>&nbsp;</p>
+      <p>&nbsp;</p>
+      <p>&nbsp;</p>
+      <p>&nbsp;</p>
+      <center>
+        <h1 className="text-3xl">Past Featured Brands </h1>
+      </center>
+      <p>&nbsp;</p>
+      <CountrySlider />
+      <p>&nbsp;</p>
+      <p>&nbsp;</p>
+      <p>&nbsp;</p>
+      <p>&nbsp;</p>
+      <center>
+        <h1 className="text-3xl">Industry Bodies Endorsing WOFX</h1>
+      </center>
+      <p>&nbsp;</p>
+      <Industribody />
+      <p>&nbsp;</p>
+      {/* <Industrypartnerslider /> */}
+
+      <center>
+        <h1 className="text-3xl py-6">International Pavilion Organisers </h1>
+      </center>
+      <InternationPavilion />
+      <p>&nbsp;</p>
     </>
   );
 }
