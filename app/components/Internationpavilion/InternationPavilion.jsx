069fe3ad05b113cfb35e5b827bb323e3385c633a
finesse-mumbai <web@finesse.co.in>
2024-07-11 11:47:30 +0530

    adding slider at internation pavillion

--- app/components/Internationpavilion/InternationPavilion.jsx
+++ app/components/Internationpavilion/InternationPavilion.jsx
@@ -1,29 +1,23 @@
-import Image from "next/image"
+'use client'
+import React from 'react';
+import Slider from "react-slick";
+import Image from "next/image";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 
-import bai from '../../../public/assests/orgnization/5.webp'
-import HRAWI from '../../../public/assests/orgnization/8.webp'
-import IICCI from '../../../public/assests/orgnization/12.webp'
-import AESA from '../../../public/assests/orgnization/9.webp'
-import CIE from '../../../public/assests/orgnization/11.webp'
-import MHEA from '../../../public/assests/orgnization/13.webp'
-import AFA from '../../../public/assests/orgnization/6.webp'
-import hawa from  '../../../public/assests/orgnization/hawa.png'
-import hawaexpo from  '../../../public/assests/orgnization/hawaexpo.png'
-import HIMKILogo from  '../../../public/assests/orgnization/HIMKILogo.png'
-import ifx from  '../../../public/assests/orgnization/IFEXLogo.png'
-import thti from  '../../../public/assests/internationalpavilion/F.png'
-import designobject from  '../../../public/assests/orgnization/designobjects.png'
-
-
-
-
-
-
-
-
-
-
-
+import bai from '../../../public/assests/orgnization/5.webp';
+import HRAWI from '../../../public/assests/orgnization/8.webp';
+import IICCI from '../../../public/assests/orgnization/12.webp';
+import AESA from '../../../public/assests/orgnization/9.webp';
+import CIE from '../../../public/assests/orgnization/11.webp';
+import MHEA from '../../../public/assests/orgnization/13.webp';
+import AFA from '../../../public/assests/orgnization/6.webp';
+import hawa from '../../../public/assests/orgnization/hawa.png';
+import hawaexpo from '../../../public/assests/orgnization/hawaexpo.png';
+import HIMKILogo from '../../../public/assests/orgnization/HIMKILogo.png';
+import ifx from '../../../public/assests/orgnization/IFEXLogo.png';
+import thti from '../../../public/assests/orgnization/thti.webp';
+import designobject from '../../../public/assests/orgnization/designobjects.png';
 
 const logoses = [{
   logo: bai,
@@ -40,8 +34,7 @@ const logoses = [{
 {
   logo: AESA,
   link: "https://aesapune.org/"
-}
-  ,
+},
 {
   logo: CIE,
   link: "https://cieonline.org/"
@@ -53,57 +46,82 @@ const logoses = [{
 {
   logo: AFA,
   link: "https://www.afa1983.com/"
-}
-,
+},
 {
   logo: hawa,
   link: "https://www.hawaexpo.com/"
-}
-,
+},
 {
   logo: hawaexpo,
   link: "https://hawa.vn/"
-}
-,
+},
 {
   logo: HIMKILogo,
   link: "https://www.himki.id/"
-}
-,
+},
 {
   logo: ifx,
   link: "https://ifexindonesia.com/"
-}
-,
+},
 {
   logo: thti,
   link: "https://www.thaitextile.org/th/home/"
 },
 {
-  logo:designobject,
-  link:"https://www.designandobjects.com/"
-}
+  logo: designobject,
+  link: "https://www.designandobjects.com/"
+}];
 
-]
+const InternationPavilion = () => {
+  const settings = {
+    infinite: true,
+    speed: 500,
+    slidesToShow: 5,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 2000,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 1,
+          infinite: true,
+          dots: true
+        }
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 1
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 1
+        }
+      }
+    ]
+  };
 
-
-
-
-function InternationPavilion() {
   return (
-    <>
-      <div className="flex  md:px-24  gap-x-2 flex-wrap justify-center">
+    <div className="md:px-24 px-1 overflow-hidden ">
+      <center>
+        <Slider {...settings}>
         {logoses.map((item, index) => (
-          <div key={index}>
-            <a target="_blank" href={item.link}>
-              <Image src={item.logo} width={220} priority={true} height='auto' alt={index} />
+          <div key={index} className="px-2">
+            <a target="_blank" href={item.link} rel="noopener noreferrer">
+              <Image src={item.logo} width={220} height={220} priority={true} alt={`logo-${index}`} />
             </a>
           </div>
         ))}
-      </div>
+      </Slider>
+      </center>
+    </div>
+  );
+};
 
-    </>
-  )
-}
-
-export default InternationPavilion
+export default InternationPavilion;
