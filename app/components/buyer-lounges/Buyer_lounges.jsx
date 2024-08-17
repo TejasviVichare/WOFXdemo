67567f928713d083958e861ddebf90a847e9f345
finesse-mumbai <web@finesse.co.in>
2024-08-17 14:46:46 +0530

    icons image

--- app/components/buyer-lounges/Buyer_lounges.jsx
+++ app/components/buyer-lounges/Buyer_lounges.jsx
@@ -3,6 +3,9 @@ import blLogo1 from '../../../public/assests/blLogo1.png'
 import blLogo2 from '../../../public/assests/blLogo2.png'
 import blLogo3 from '../../../public/assests/blLogo3.png'
 import blLogo4 from '../../../public/assests/blLogo4.png'
+import icon from '../../../public/assests/Icon.png'
+
+
 import Link from "next/link"
 
 
@@ -22,12 +25,12 @@ respected Buyers Lounge during your visit</p>
         <div id="businessLounchCard" className="relative px-3  py-3">
            <div className="blImage"><Image   src={blLogo1} width={160} height={160} alt="Dealers & Distributors" /></div>
            <div className="py-2">
-            <div><span className="wingdings">&#x1F7C2;</span> Dealers</div>
-            <div><span className="wingdings">&#x1F7C2;</span> Distributors</div>
-            <div><span className="wingdings">&#x1F7C2;</span> Wholesalers</div>
-            <div><span className="wingdings">&#x1F7C2;</span> Importers</div>
-            <div><span className="wingdings">&#x1F7C2;</span> Trading & Buying Houses</div>
-            <div><span className="wingdings">&#x1F7C2;</span> Agents</div>
+            <div className="flex items-center gap-1"><span className="wingdings"><Image   src={icon} width={15} height={15} alt="icons" /></span> Dealers</div>
+            <div className="flex items-center gap-1"><span className="wingdings"><Image   src={icon} width={15} height={15} alt="icons" /></span> Distributors</div>
+            <div className="flex items-center gap-1"><span className="wingdings"><Image   src={icon} width={15} height={15} alt="icons" /></span> Wholesalers</div>
+            <div className="flex items-center gap-1"><span className="wingdings"><Image   src={icon} width={15} height={15} alt="icons" /></span> Importers</div>
+            <div className="flex items-center gap-1"><span className="wingdings"><Image   src={icon} width={15} height={15} alt="icons" /></span> Trading & Buying Houses</div>
+            <div className="flex items-center gap-1"><span className="wingdings"><Image   src={icon} width={15} height={15} alt="icons" /></span> Agents</div>
            </div>
           
         </div>
@@ -36,32 +39,32 @@ respected Buyers Lounge during your visit</p>
            <Image   src={blLogo4} width={160} height={160} alt="Dealers & Distributors" />
            </div>
            <div className="py-2">
-            <div><span className="wingdings">&#x1F7C2;</span> Retailers</div>
-            <div><span className="wingdings">&#x1F7C2;</span> E-Tailers</div>
-            <div><span className="wingdings">&#x1F7C2;</span> Large Format Retailers</div>
-            <div><span className="wingdings">&#x1F7C2;</span> Online Traders</div>
+            <div className="flex items-center gap-1"><span className="wingdings"><Image   src={icon} width={15} height={15} alt="icons" /></span> Retailers</div>
+            <div className="flex items-center gap-1"><span className="wingdings"><Image   src={icon} width={15} height={15} alt="icons" /></span> E-Tailers</div>
+            <div className="flex items-center gap-1"><span className="wingdings"><Image   src={icon} width={15} height={15} alt="icons" /></span> Large Format Retailers</div>
+            <div className="flex items-center gap-1"><span className="wingdings"><Image   src={icon} width={15} height={15} alt="icons" /></span> Online Traders</div>
            </div>
            
         </div>
         <div id="businessLounchCard" className="relative px-3  py-3">
            <div className="blImage"><Image   src={blLogo3} width={160} height={160} alt="Dealers & Distributors" /></div>
            <div className="py-2">
-            <div><span className="wingdings">&#x1F7C2;</span> Franchisees</div>
-            <div><span className="wingdings">&#x1F7C2;</span> Entrepreneurs</div>
-            <div><span className="wingdings">&#x1F7C2;</span> Agents</div>
-            <div><span className="wingdings">&#x1F7C2;</span> Consultants</div>
+            <div className="flex items-center gap-1"><span className="wingdings"><Image   src={icon} width={15} height={15} alt="icons" /></span> Franchisees</div>
+            <div className="flex items-center gap-1"><span className="wingdings"><Image   src={icon} width={15} height={15} alt="icons" /></span> Entrepreneurs</div>
+            <div className="flex items-center gap-1"><span className="wingdings"><Image   src={icon} width={15} height={15} alt="icons" /></span> Agents</div>
+            <div className="flex items-center gap-1"><span className="wingdings"><Image   src={icon} width={15} height={15} alt="icons" /></span> Consultants</div>
            </div>
           
         </div>
         <div id="businessLounchCard" className="relative px-3  py-3">
            <div className="blImage"><Image   src={blLogo2} width={160} height={160} alt="Dealers & Distributors" /></div>
            <div className="py-2">
-            <div><span className="wingdings">&#x1F7C2;</span> Architects</div>
-            <div><span className="wingdings">&#x1F7C2;</span> Interior Designers</div>
-            <div><span className="wingdings">&#x1F7C2;</span> Real Estate Developers</div>
-            <div><span className="wingdings">&#x1F7C2;</span> HORECA</div>
-            <div><span className="wingdings">&#x1F7C2;</span> Corporate Sourcing Heads</div>
-            <div><span className="wingdings">&#x1F7C2;</span> Institutional</div>
+            <div className="flex items-center gap-1"><span className="wingdings"><Image   src={icon} width={15} height={15} alt="icons" /></span> Architects</div>
+            <div className="flex items-center gap-1"><span className="wingdings"><Image   src={icon} width={15} height={15} alt="icons" /></span> Interior Designers</div>
+            <div className="flex items-center gap-1"><span className="wingdings"><Image   src={icon} width={15} height={15} alt="icons" /></span> Real Estate Developers</div>
+            <div className="flex items-center gap-1"><span className="wingdings"><Image   src={icon} width={15} height={15} alt="icons" /></span> HORECA</div>
+            <div className="flex items-center gap-1"><span className="wingdings"><Image   src={icon} width={15} height={15} alt="icons" /></span> Corporate Sourcing Heads</div>
+            <div className="flex items-center gap-1"><span className="wingdings"><Image   src={icon} width={15} height={15} alt="icons" /></span> Institutional</div>
            </div>
            
         </div>
