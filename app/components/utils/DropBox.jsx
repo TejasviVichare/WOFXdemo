843bfa3188cba076281ed07ea7329e6bfbe25f63
TejasviVichare <tejasvivichare2121@gmail.com>
2024-08-24 12:41:03 +0530

    Newsletter text Updated

--- app/components/utils/DropBox.jsx
+++ app/components/utils/DropBox.jsx
@@ -7,7 +7,7 @@ function DropBox() {
       <div className='dropbox'>
        <a href="/assests/pdf/Newsletter.pdf">
        <div className='flex justify-center gap-4 items-center'>
-          <div className='text-lg text-white leading-tight'>Download <br /> Newsletter</div>
+          <div className='text-lg text-white leading-tight text-center'>Download <br /> F+D Newsletter</div>
           <FontAwesomeIcon className=' h-14 text-white' icon={faFileLines} />
         </div>
        </a>
