dfe37d96bf3a2b0c6c0708d89f7f6de0c512649b
finesse-mumbai <web@finesse.co.in>
2024-10-23 13:51:57 +0530

    making url short

---
+++ app/Click-Here/page.jsx
@@ -0,0 +1,18 @@
+
+'use client';
+import { useEffect } from 'react';
+
+
+ const Page= () => {
+  useEffect(() => {
+     window.location.href  = 'https://www.bee2bee.asia/wofx/buyer-registration?ref=HPMF';
+  }, []);
+  return(  
+  <>
+   {/* <div className='h-full text-center py-4'>
+       <p className="text-red-600">Registration is now closed</p>
+   </div> */}
+   </>)
+
+};
+export default Page
