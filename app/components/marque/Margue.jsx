6701da2580bb82c5ad97c41c2c1bc5363ed9a4e4
finesse-mumbai <web@finesse.co.in>
2024-11-13 16:57:25 +0530

    adding morque in website

--- app/components/marque/Margue.jsx
+++ app/components/marque/Margue.jsx
@@ -2,39 +2,26 @@ import React from 'react'
 import Marquee from 'react-fast-marquee'
 
 export default function Margue() {
+
+    const logoFilenames = [
+        "1.png", "2.png", "3.png", "3A.png", "3B.png", "3C.png", "4.png", "5.png", 
+        "6.png", "7.png", "8.png", "9.png", "10.png", "11.png", "12.png", "13.png", 
+        "14.png", "15.png", "16.png", "17.png", "18.png", "19.png", "20.png", 
+        "21.png", "22.png", "23.png", "24.png", "25.png", "26.png", "27.png", 
+        "28.png", "29.png", "30.png", "31.png", "32.png", "33.png", "34.png", 
+        "35.png", "36.png", "37.png", "38.png", "39.png", "40.png", "41.png", 
+        "42.png", "43.png", "44.png", "45.png", "46.png", "47.png", "48.png", 
+        "49.png", "50.png"
+      ];
+
   return (
     <div>
         <Marquee direction="left" pauseOnHover={true} speed={100} delay={5} >
-          <div className="image_wrapper">
-            <img src='/assests/countyLogo/1.png' alt="" />
-          </div>
-          <div className="image_wrapper">
-            <img src='/assests/countyLogo/2.png' alt="" />
-          </div>
-          <div className="image_wrapper">
-            <img src='/assests/countyLogo/3.png' alt="" />
-          </div>
-          <div className="image_wrapper">
-            <img src='/assests/countyLogo/3A.png' alt="" />
-          </div>
-          <div className="image_wrapper">
-            <img src='/assests/countyLogo/3B.png' alt="" />
-          </div>
-          <div className="image_wrapper">
-            <img src='/assests/countyLogo/3C.png' alt="" />
-          </div>
-          <div className="image_wrapper">
-            <img src='/assests/countyLogo/4.png' alt="" />
-          </div>
-          <div className="image_wrapper">
-            <img src='/assests/countyLogo/5.png' alt="" />
-          </div>
-          <div className="image_wrapper">
-            <img src='/assests/countyLogo/6.png' alt="" />
-          </div>
-          <div className="image_wrapper">
-            <img src='/assests/countyLogo/7.png' alt="" />
-          </div>
+        {logoFilenames.map((filename, index) => (
+                    <div className="image_wrapper" key={index}>
+                        <img src={`/assests/countyLogo/${filename}`} alt={`Logo ${index + 1}`} />
+                    </div>
+                ))}
         </Marquee>
       </div>
   )
