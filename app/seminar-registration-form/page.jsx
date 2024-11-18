07576d5c968b308df6d4b5f2e8c102a55a595402
finesse-mumbai <web@finesse.co.in>
2024-11-18 18:56:25 +0530

    adding seminar registration form

---
+++ app/seminar-registration-form/page.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+export default function page() {
+  return (
+    <div className="mx-auto   w-full max-w-screen-2xl mt-40 ">
+      <div className="mt-16  pb-24">
+        <h1 className="text-xl md:text-3xl py-2 font-semibold text-center">
+          Seminar Registration Form
+        </h1>
+       
+        <p>&nbsp;</p>
+        <p>&nbsp;</p>
+        <p>&nbsp;</p>
+        <p>&nbsp;</p>
+      </div>
+    </div>
+  )
+}
