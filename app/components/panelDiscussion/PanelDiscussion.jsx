ea7505b655778bab35d7f79e2d4776fb4870b802
finesse-mumbai <web@finesse.co.in>
2024-11-21 18:22:49 +0530

    adding smart talks

--- app/components/panelDiscussion/PanelDiscussion.jsx
+++ app/components/panelDiscussion/PanelDiscussion.jsx
@@ -16,11 +16,11 @@ export default function PanelDiscussion() {
         <div className="mx-auto w-full max-w-screen-2xl mt-32 overflow-hidden" onMouseMove={handleMouseMove}>
             <div className="py-4 px-4 md:px-44 md:py-12">
 
-                <div className=" border-r border-b relative z-50 overflow-hidden"  >
+                <div className=" border-r border-b relative z-50 overflow-hidden panelMainBox"  >
 
-                    <div className="absolute inset-0 z-0 pointer-events-none">
+                    {/* <div className="absolute inset-0 z-0 pointer-events-none">
                         <Backgroundpattern />
-                    </div>
+                    </div> */}
 
                     <div
                         className="absolute"
@@ -45,7 +45,7 @@ export default function PanelDiscussion() {
                         height="250"
                         alt="Business Talk"
                     />
-                    <div class=" relative z-50 grid grid-cols-1 sm:grid-cols-3 gap-8 px-4 py-4">
+                    <div class=" relative z-50 grid grid-cols-1 sm:grid-cols-3 gap-6 px-4 py-4">
                         <div class=" speakerBox  rounded-lg flex flex-col justify-center items-center p-4 w-full h-60">
                             <Image className="rounded-t-lg mb-2" src="/assests/speaker (1).png" width="100" height="250" alt="Speaker Image" />
                             <div class="text-center">
