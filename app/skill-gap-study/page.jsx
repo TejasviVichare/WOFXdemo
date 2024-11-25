54cb46d6d928cda3d7cc0d1426d2b485ff3633c4
finesse-mumbai <web@finesse.co.in>
2024-11-25 11:16:49 +0530

    form fixing

--- app/skill-gap-study/page.jsx
+++ app/skill-gap-study/page.jsx
@@ -110,7 +110,7 @@ const SkillForm = () => {
                 ...values,
                 category: values.category ? values.category.value : '',
                 state: values.state ? values.state.value : '',
-                positions: values.positions.map(pos => pos.positionName).join(', '), // Adjust based on your actual object structure
+                // positions: values.positions.map(pos => pos.positionName, pos.positionNumber).join(', '), 
             };
 
             console.log(submissionData, "form Data for submit");
