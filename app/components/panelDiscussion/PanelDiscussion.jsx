e805977254624e40d3c9c872d3f3567488548700
finesse-mumbai <web@finesse.co.in>
2024-11-27 17:19:15 +0530

    changing the eslintrc

--- app/components/panelDiscussion/PanelDiscussion.jsx
+++ app/components/panelDiscussion/PanelDiscussion.jsx
@@ -1,42 +1,17 @@
-"use client";
-import React, { useState } from "react";
+
+import React from "react";
 import Image from "next/image";
-import Backgroundpattern from "../backgroundPattern/Backgroundpattern";
 import Link from "next/link";
 
 export default function PanelDiscussion() {
-    const [position, setPosition] = useState({ x: 0, y: 0 });
-
-    const handleMouseMove = (e) => {
-        const { clientX, clientY } = e;
-        console.log(clientX, clientY)
-        setPosition({ x: clientX, y: clientY });
-    };
+ 
     return (
-        <div className="mx-auto w-full max-w-screen-2xl mt-32 overflow-hidden" onMouseMove={handleMouseMove}>
+        <div className="mx-auto w-full max-w-screen-2xl mt-32 overflow-hidden" >
             <div className="py-4 px-4 md:px-44 md:py-12">
 
                 <div className=" border-r border-b relative  overflow-hidden panelMainBox"  >
 
-                    {/* <div className="absolute inset-0 z-0 pointer-events-none">
-                        <Backgroundpattern />
-                    </div> */}
-
-                    <div
-                        className="absolute"
-                        style={{
-                            width: '500px',
-                            height: '500px',
-                            borderRadius: '100%',
-                            backgroundImage: 'radial-gradient(circle, #a6ccfb, #afd0fa, #b8d5f9, #c0d9f8, #c9ddf7, #d1e1f7, #d9e5f8, #e1e9f8, #eaeefa, #f2f3fb, #f9f9fd, #ffffff)',
-                            filter: 'blur(30px)',
-                            position: 'absolute',
-                            zIndex: "-3",
-                            top: position.y + 250, // Offset for centering the gradient
-                            left: position.x - 400, // Offset for centering the gradient
-                            pointerEvents: 'none', // Ensures it doesn't block mouse interaction
-                        }}
-                    />
+                 
 
                     <Image
                         className="rounded-t-lg relative "
