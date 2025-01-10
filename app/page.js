e3caedfe6d274f090eb10618051991660882a17f
finesse-mumbai <web@finesse.co.in>
2025-01-10 11:44:40 +0530

    changing testinomial

--- app/page.js
+++ app/page.js
@@ -1,22 +1,13 @@
 import dynamic from 'next/dynamic';
 import { Suspense } from 'react';
-// import Aboutwofx from "./components/aboutwofx/Aboutwofx.jsx";
 import Wofxadvantage from "./components/wofxadvantage/Wofxadvantage.jsx";
- import CountrySlider from "./components/countryslider/CountrySlider.jsx";
+
 import Industribody from "./components/industribodies/Industribody.jsx";
- import InternationPavilion from "./components/Internationpavilion/InternationPavilion.jsx";
 import Buyer_lounges from "./components/buyer-lounges/Buyer_lounges.jsx";
-import Show_Summery from "./components/show-summery/Show_Summery.jsx";
 import FixedSocial from "./components/FixedSocial/FixedSocial.jsx";
 import DropBox from './components/utils/DropBox.jsx';
-import RegsiterButton from './components/utils/RegsiterButton.jsx';
-import PreviewPopup from './components/utils/PreviewPopup.jsx';
-
 import Margue from './components/marque/Margue.jsx';
 import DropBoxPSR from './components/utils/DropBoxPSR.jsx';
-
-
-// import Industrypartnerslider from "./components/industrypartnerslider/Industrypartnerslider.jsx";
 const Industrypartnerslider = dynamic(() => import('./components/industrypartnerslider/Industrypartnerslider.jsx'), {
   suspense: true,
 });
@@ -27,34 +18,31 @@ const Aboutwofx = dynamic(() => import('./components/aboutwofx/Aboutwofx.jsx'), 
 export default function Home() {
   return (
     <>
-        <div className="videoContainer">
-          <div className="bgvideo mx-auto  w-full max-w-screen-2xl">
-            <video
-              muted
-              autoPlay
-              playsInline
-              loop
-             
-            >
-              <source src="assests/WOFX_4_nifcf6.mp4" type="video/mp4" />
-            </video>
+      <div className="videoContainer">
+        <div className="bgvideo mx-auto  w-full max-w-screen-2xl">
+          <video
+            muted
+            autoPlay
+            playsInline
+            loop
+
+          >
             <source src="assests/WOFX_4_nifcf6.mp4" type="video/mp4" />
-          </div>
+          </video>
+          <source src="assests/WOFX_4_nifcf6.mp4" type="video/mp4" />
         </div>
-    
+      </div>
+
       <p>&nbsp;</p>
       <p>&nbsp;</p>
       <p>&nbsp;</p>
       <p>&nbsp;</p>
 
-   
+
       <Suspense fallback={<div>Loading...</div>}>
         <Aboutwofx />
       </Suspense>
-      {/* <Aboutwofx /> */}
       <p>&nbsp;</p>
-      
-      {/* <Show_Summery /> */}
       <p>&nbsp;</p>
       <p>&nbsp;</p>
 
@@ -65,28 +53,16 @@ export default function Home() {
       <center>
         <h1 className="text-3xl">Brands Connect @WOFX 2024 </h1>
       </center>
-   
-{/*       
-      <CountrySlider /> */}
-    
-
-   
       <p>&nbsp;</p>
-
-<Margue />
-<p>&nbsp;</p>
+      <Margue />
+      <p>&nbsp;</p>
       <p>&nbsp;</p>
       <p>&nbsp;</p>
       <Buyer_lounges />
       <p>&nbsp;</p>
       <p>&nbsp;</p>
-      {/* <p>&nbsp;</p>
-      <p>&nbsp;</p>
-      <p>&nbsp;</p> */}
-      
       <p>&nbsp;</p>
       <p>&nbsp;</p>
- 
       <center>
         <h3 className="text-3xl">Industry Bodies Endorsing WOFX</h3>
       </center>
@@ -97,23 +73,20 @@ export default function Home() {
       <Suspense fallback={<div>Loading...</div>}>
         <Industrypartnerslider />
       </Suspense>
-
-   
       <p>&nbsp;</p>
-        
-        <p>&nbsp;</p>
-        <div className='relative'>
+      <p>&nbsp;</p>
+      <div className='relative'>
         <div className='fixed bottom-[125px] right-5'>
-        <DropBoxPSR />
+          <DropBoxPSR />
         </div>
-        </div>
-        <div className='relative'>
+      </div>
+      <div className='relative'>
         <div className='fixed bottom-5 right-5'>
-        <DropBox />
+          <DropBox />
         </div>
-        </div>
+      </div>
       <FixedSocial />
-      
+
     </>
   );
 }
