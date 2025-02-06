131398a28653442d1579e9cac26ed47f62315500
finesse-mumbai <web@finesse.co.in>
2025-02-06 16:45:55 +0530

    changing pdf

--- app/components/aboutwofx/Aboutwofx.jsx
+++ app/components/aboutwofx/Aboutwofx.jsx
@@ -13,24 +13,17 @@ function Aboutwofx() {
             <p className="text-justify md:pl-8"><strong>WOFX - World Furniture Expo</strong> is India&apos;s premier international B2B trade show dedicated to the furniture + design industry wherein the entire furniture + design industry from India and across the globe can converge, connect and collaborate. WOFX is your gateway into the vast Indian furniture & design market. </p>
             <div id="atWofx" className=" flex flex-wrap gap-4 mt-8">
               <Image src={aboutHomePage} width='90%' height='auto' alt="something" />
-              {/* <div><p><span>&#x27A4; &nbsp;</span> Accelerate <strong className="text-xl">Brand Presence</strong></p></div>
-              <div><p><span>&#x27A4; &nbsp;</span> Access Leading Buyers</p></div>
-              <div><p><span>&#x27A4; &nbsp;</span> Generate Leads</p></div>
-              <div><p><span>&#x27A4; &nbsp;</span> Expand <strong className="text-xl">Business Network</strong></p></div>
-              <div><p><span>&#x27A4; &nbsp;</span> Gain Market Insights</p></div> */}
+              
             </div>
-            {/* <div id="aboutwofxbut" className="flex flex-wrap gap-4 justify-center mt-10">
-             <Link className="BLButton"  href=''><div>For Booth Participation - <span>Click Here</span></div></Link>
-             <Link  className="BLButton" href=''><div>For Buyer Registration - <span>Click Here</span></div></Link>
-            </div> */}
+    
           </div>
           <div id="glimpsessVideo">
         
             <div className="pt-2">
             <Suspense fallback={"Loading...."}>
-            <div style={{ position: 'relative', width: '100%', paddingBottom: '56.25%', height: 0 }}>
-              <iframe
-                src="https://www.youtube.com/embed/jCSVmpPf03E?si=RkXa8Bldjdp6JDKK"
+            <div className="rounded" style={{ position: 'relative', width: '100%', paddingBottom: '56.25%', height: 0 }}>
+              <iframe className="rounded"
+                src="https://www.youtube.com/embed/iFVLLPsucw4?si=Kmvv0uqbZPv6H9Zx"
                 title="Embedded Video"
                 allowFullScreen
                 style={{ position: 'absolute', width: '100%', height: '100%', top: 0, left: 0 }}
@@ -44,5 +37,8 @@ function Aboutwofx() {
     </>
   )
 }
+
+
+
 
 export default Aboutwofx
