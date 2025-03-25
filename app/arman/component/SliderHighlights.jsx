aadc3cdc063939702b40c2530638e4b06bcdfa92
TejasviVichare <tejasvivichare2121@gmail.com>
2025-03-25 15:38:05 +0530

    Autospeed Increased

--- app/arman/component/SliderHighlights.jsx
+++ app/arman/component/SliderHighlights.jsx
@@ -21,7 +21,7 @@ const SliderHighlights = () => {
     slidesToShow: 1,
     slidesToScroll: 1,
     autoplay: true,
-    autoplaySpeed: 3000,
+    autoplaySpeed: 2000,
     arrows: false,
     responsive: [
       {
